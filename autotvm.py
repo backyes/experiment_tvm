dcc53fd13efe36ef9f90470088faf83486f8c6a3
root <root@ml5.jd.163.org>
2022-10-03 08:44:53 +0000

    Add tune_relay_x86.py

--- autotvm.py
+++ autotvm.py
@@ -4,8 +4,8 @@ import numpy as np
 
 import tvm
 from tvm import te
-import topi
-from topi.testing import conv2d_nchw_python
+from tvm import topi
+from tvm.topi.testing import conv2d_nchw_python
 
 from tvm import autotvm
 
@@ -18,7 +18,17 @@ data = te.placeholder((N, CI, H, W), name='data')
 kernel = te.placeholder((CO, CI, KH, KW), name='kernel')
 conv = topi.nn.conv2d_nchw(data, kernel, strides, padding, dilation=1, out_dtype='float32')
 #cfg = autotvm.get_config()
-task = autotvm.task.create("conv2d_nchw.cuda",
-                           args=([conv],),
-                           target='cuda')
+task = autotvm.task.create("conv2d_nchw",
+                           args=(data, kernel, strides, padding, 1, 'float32'),
+                           target='llvm')
 print(task.config_space)
+
+measure_option = autotvm.measure_option(
+    builder=autotvm.LocalBuilder(),
+    runner=autotvm.LocalRunner(repeat=3, min_repeat_ms=100, timeout=4)
+)
+
+tuner = autotvm.tuner.XGBTuner(task)
+tuner.tune(n_trial=20,
+           measure_option=measure_option,
+           callbacks=[autotvm.callback.log_to_file('conv2d.log')])
